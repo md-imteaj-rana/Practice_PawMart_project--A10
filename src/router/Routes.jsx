649d681ab11e492ca8e3b95f60f6e42d05384bd4
MD. Imteaj Rana <imteaj.rana2@gmail.com>
2025-12-05 19:08:07 +0600

    4th

--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -1,9 +1,18 @@
 import { createBrowserRouter } from "react-router";
+import RootLayout from "../root/RootLayout";
+import Home from "../pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <RootLayout></RootLayout>,
+    children:[
+        {
+            path: "/",
+            element: <Home></Home>
+        }
+
+    ]
   },
 ]);
 
